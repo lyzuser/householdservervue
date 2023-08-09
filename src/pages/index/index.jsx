dda5594a156deacec8469d5447cebbbdff698252
SY <1719884506@qq.com>
2023-08-09 10:28:54 +0800

    首页0.1.0版本，初步加入部分元素

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,100 +1,55 @@
-import {Component} from "react";
-import {View, Swiper, SwiperItem, ScrollView} from '@tarojs/components'
+import { View, Text , Swiper, SwiperItem } from '@tarojs/components'
+import { useLoad } from '@tarojs/taro'
 import Taro from "@tarojs/taro";
 import './index.scss'
-import TabBar from '../common/Index'
+import TabBar from '../common/Index';
+import { AtGrid } from "taro-ui"
 
 
-export default class Index extends Component {
+export default function Index() {
+  useLoad(() => {
+    console.log('Page loaded.')
+  })
 
-  constructor() {
-    super();
-    this.state={
-      productList:[]
-    }
-  }
-  componentDidMount() {
-    let _this=this
-    Taro.request({
-      url: 'https://www.juntaitec.cn/api/productList', //仅为示例，并非真实的接口地址
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: function (res) {
-        console.log(res.data)
-        _this.setState({
-          productList:res.data.data
-        })
-      }
-    })
-  }
-  onScrollToUpper() {}
-
-  // or 使用箭头函数
-  // onScrollToUpper = () => {}
-
-  onScroll(e){
-    console.log(e.detail)
-  }
-
-  render() {
-
-    const scrollTop = 0
-    const Threshold = 20
-
-    return (
-      <View className='index'>
-        <Swiper
-          className='swiper'
-          indicatorColor='#999'
-          indicatorActiveColor='#333'
-          vertical={false}
-          circular
-          indicatorDots
-          autoplay
-        >
-          <SwiperItem className='swiper-item'>
-            <image src='http://43.139.94.243/images/ban1.jpg'></image>
-          </SwiperItem>
-          <SwiperItem className='swiper-item'>
-            <image src='http://43.139.94.243/images/ban2.jpg'></image>
-          </SwiperItem>
-          <SwiperItem className='swiper-item'>
-            <image src='http://43.139.94.243/images/ban3.jpg'></image>
-          </SwiperItem>
-        </Swiper>
-        <ScrollView
-          className='scrollview'
-          scrollY
-          enableFlex='true'
-          scrollWithAnimation
-          scrollTop={scrollTop}
-          lowerThreshold={Threshold}
-          upperThreshold={Threshold}
-          onScrollToUpper={this.onScrollToUpper.bind(this)} // 使用箭头函数的时候 可以这样写 `onScrollToUpper={this.onScrollToUpper}`
-          onScroll={this.onScroll}
-        >
+  return (
+    <View className='index'>
+      <Text>三体家政服务小程序</Text>
+      <AtGrid mode='rect' data={
+        [
           {
-            this.state.productList.map((product)=>{
-              return(
-                <View className='goods-box'>
-                  <image className='goods-pic' src={product.src}></image>
-                  <View className='goods-title'>{product.title}</View>
-                  <View className='goods-titleTwo'>{product.secondtitle}</View>
-                  <View className='row'>
-                    <View className='goods-price'>{product.price}</View>
-                    <View className='goods-btn'>看相似</View>
-                  </View>
-                </View>
-              )
-            })
+            image: 'http://43.139.94.243/icon/sort.png',
+            value: '小黑板',
+
+          },
+          {
+            image: 'http://43.139.94.243/icon/cart.png',
+            value: '服务介绍'
+          },
+          {
+            image: 'http://43.139.94.243/icon/sort.png',
+            value: '保洁清洗'
+          },
+          {
+            image: 'http://43.139.94.243/icon/sort.png',
+            value: '保姆月嫂'
+          },
+          {
+            image: 'http://43.139.94.243/icon/sort.png',
+            value: '家庭维修'
+          },
+          {
+            image: 'http://43.139.94.243/icon/sort.png',
+            value: '搬家安装'
           }
+        ]
+      }
+      />
 
-        </ScrollView>
-        <TabBar current={0}></TabBar>
-      </View>
-    )
-  }
+      <TabBar current={0}></TabBar>
 
 
+    </View>
+
+
+  )
 }
